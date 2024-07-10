c218751429ca57484d871cb4c66a29c4bdece26f
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-10 13:20:44 +0200

    chore: Update homepage layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import GiftForm from '../components/gift-form'
 
 export default async function HomePage() {
-  return <GiftForm />
+  return (
+    <div className='bg-blue-500 h-[500px]'>
+      <div className='relative mx-auto max-w-7xl'>
+        <h1 className='text-6xl font-bold text-white max-w-[500px] leading-[72px] py-32'>Generador de regalos con ia</h1>
+        <GiftForm />
+      </div>
+    </div>
+  )
 }
--- src/components/gift-form.tsx
+++ src/components/gift-form.tsx
@@ -6,22 +6,19 @@ export default function GiftForm() {
   const { messages, input, handleSubmit, handleInputChange } = useChat()
 
   return (
-    <div className='flex flex-col w-full max-w-md mx-auto stretch p-20'>
-      {messages.map(m => (
-        <div key={m.id} className='whitespace-pre-wrap'>
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
+    <section className='absolute flex justify-between flex-col w-[700px] p-4 mx-auto border shadow-xl right-0 top-32 h-[1000px] bg-white rounded-lg'>
+      <div>
+        {messages.map(m => (
+          <div key={m.id} className='whitespace-pre-wrap'>
+            {m.role === 'user' ? 'User: ' : 'AI: '}
+            {m.content}
+          </div>
+        ))}
+      </div>
 
       <form onSubmit={handleSubmit}>
-        <input
-          className='fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl'
-          value={input}
-          placeholder='Say something...'
-          onChange={handleInputChange}
-        />
+        <input className='w-full p-2 border border-gray-300 rounded shadow-md' value={input} placeholder='Say something...' onChange={handleInputChange} />
       </form>
-    </div>
+    </section>
   )
 }

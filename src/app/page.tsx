f9912781ea6c54557d3aaa5270bf3af42c0456d1
Sergi Tejada <sergitejada0@gmail.com>
2024-07-10 21:28:02 +0200

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 import GiftForm from "../components/gift-form";
 import Button from "../components/ui/button";
 import Card from "../components/ui/card";
+import { FlipWords } from "../components/ui/flip-words";
 
+const words = ["Padre", "Esposa", "Amigos", "Familia"];
 export default async function HomePage() {
   return (
     <div className="bg-blue-500 h-[500px]">
-      <div className="relative mx-auto max-w-7xl flex ">
-        <h1 className="text-6xl font-bold text-white max-w-[500px] leading-[72px] py-32">
-          Sorprende a [Nombre del Destinatario] con el Regalo Ideal
-        </h1>
+      <div className="relative mx-auto max-w-7xl flex justify-center gap-10 pt-20">
+        <div className="h-[40rem] flex justify-center items-center px-4">
+          <div className="text-4xl mx-auto font-bold text-white">
+            Sorprende a tu
+            <FlipWords words={words} /> <br />
+            con el Regalo Ideal
+          </div>
+        </div>
+
         {/* <GiftForm /> */}
         <Card>
           <h1 className="text-6xl font-bold text-white>Generador de">AAAAAA</h1>

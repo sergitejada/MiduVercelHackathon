c5ae5e5ab1a91b0e73c01d7974778318a2c8d593
Sergi Tejada <sergitejada0@gmail.com>
2024-07-10 15:29:00 +0200

    card

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,9 @@
+import styles from "./card.module.css";
+
+interface CardProps {}
+
+function Card({ children }: React.PropsWithChildren<CardProps>) {
+  return <div className={styles.card}>{children}</div>;
+}
+
+export default Card;

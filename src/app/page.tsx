7f13e488aa92d58457b3d13e9a7db8f8209f9a13
Sergi Tejada <sergitejada0@gmail.com>
2024-07-10 15:29:47 +0200

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import GiftForm from '../components/gift-form'
+import GiftForm from "../components/gift-form";
+import Button from "../components/ui/button";
+import Card from "../components/ui/card";
 
 export default async function HomePage() {
   return (
-    <div className='bg-blue-500 h-[500px]'>
-      <div className='relative mx-auto max-w-7xl'>
-        <h1 className='text-6xl font-bold text-white max-w-[500px] leading-[72px] py-32'>Generador de regalos con ia</h1>
-        <GiftForm />
+    <div className="bg-blue-500 h-[500px]">
+      <div className="relative mx-auto max-w-7xl flex ">
+        <h1 className="text-6xl font-bold text-white max-w-[500px] leading-[72px] py-32">
+          Sorprende a [Nombre del Destinatario] con el Regalo Ideal
+        </h1>
+        {/* <GiftForm /> */}
+        <Card>
+          <h1 className="text-6xl font-bold text-white>Generador de">AAAAAA</h1>
+          <div className="w-36">
+            <Button>Test</Button>
+          </div>
+        </Card>
       </div>
     </div>
-  )
+  );
 }

0c642c8bf5f847b8fcce24476cb1194ebd0c4578
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-11 13:05:05 +0200

    feat: Add types for gift form and form step

---
+++ src/types/types.d.ts
@@ -0,0 +1,17 @@
+export type Genre = "man" | "women"
+
+export type AgeRange = "0-12" | "13-18" | "19-30" | "31-50" | "51-70" | "71-100"
+
+export type Event = "birthday" | "anniversary" | "graduation" | "wedding" | "christmas" | "valentine" | "other"
+
+export type Hobbies = "music" | "sports" | "reading" | "cooking" | "travel" | "other"
+
+export interface GiftForm {
+	genre?: Genre
+	ageRange?: AgeRange
+	event?: Event
+	hobbies?: Hobbies[]
+	moreDetails?: string
+}
+
+export type GiftFormStep = keyof GiftForm

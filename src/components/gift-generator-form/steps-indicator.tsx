5891ac0e4287f811ad33ecafa85a015abc65a5f1
Sergi Tejada <sergitejada0@gmail.com>
2024-07-11 18:32:20 +0200

    optimizar step

--- src/components/gift-generator-form/steps-indicator.tsx
+++ src/components/gift-generator-form/steps-indicator.tsx
@@ -1,31 +1,19 @@
 export default function StepsIndicator() {
+	const steps = [1, 2, 3, 4, 5]
+	const activeStep = 1 // El paso actual puede ser dinámico
+
 	return (
 		<ol className="mb-4 flex w-full items-center justify-center sm:mb-5">
-			<li className="flex w-full items-center text-black after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-neutral-200 after:content-['']">
-				<div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full border-2 border-black bg-orange-200 drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] lg:h-12 lg:w-12">
-					1
-				</div>
-			</li>
-			<li className="flex w-full items-center text-black after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-neutral-200 after:content-['']">
-				<div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full border-2 border-black bg-white drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] lg:h-12 lg:w-12">
-					2
-				</div>
-			</li>
-			<li className="flex w-full items-center text-black after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-neutral-200 after:content-['']">
-				<div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full border-2 border-black bg-white drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] lg:h-12 lg:w-12">
-					3
-				</div>
-			</li>
-			<li className="flex w-full items-center text-black after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-neutral-200 after:content-['']">
-				<div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full border-2 border-black bg-white drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] lg:h-12 lg:w-12">
-					4
-				</div>
-			</li>
-			<li className="flex items-center text-black after:inline-block">
-				<div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full border-2 border-black bg-white drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] lg:h-12 lg:w-12">
-					5
-				</div>
-			</li>
+			{steps.map((step, index) => (
+				<li key={index} className={`flex items-center ${index !== steps.length - 1 ? "w-full" : ""} text-black`}>
+					<div
+						className={`flex h-10 w-10 shrink-0 items-center justify-center rounded-full border-2 border-black ${step === activeStep ? "bg-orange-200" : "bg-white"} drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] lg:h-12 lg:w-12`}
+					>
+						{step}
+					</div>
+					{index < steps.length - 1 && <div className="h-1 w-full border-4 border-b border-neutral-200" />}
+				</li>
+			))}
 		</ol>
 	)
 }

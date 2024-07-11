7eb8806bbc9c8f25411af39503ffdaff5b086231
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-11 11:53:24 +0200

    chore: Refactor button component to use tailwind CSS classes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,11 +1,11 @@
-import styles from "./button.module.css"
-
 interface Props {
 	children: React.ReactNode
 }
 
-function Button({ children }: Props) {
-	return <button className={styles.button}>{children}</button>
+export default function Button({ children }: Props) {
+	return (
+		<button className="relative flex h-[30px] w-[90px] items-center justify-center gap-[10px] rounded-[10px] border-2 border-black bg-white p-[10px] drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] transition duration-200 hover:drop-shadow-[1px_1px_0px_rgba(0,0,0,1)]">
+			{children}
+		</button>
+	)
 }
-
-export default Button

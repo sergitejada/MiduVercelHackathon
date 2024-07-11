1c893cb617c8822b21c0cf406122a1a01a2b8c23
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-11 13:05:10 +0200

    chore: Update gift generator form store to use GiftForm and GiftFormStep types

--- src/store/gift-generator-form-store.ts
+++ src/store/gift-generator-form-store.ts
@@ -1,17 +1,36 @@
+import { GiftForm, GiftFormStep } from "@/types/types"
 import { create } from "zustand"
 
 interface GiftGeneratorFormStore {
-	gift: string
-	step: number
+	gift: GiftForm | null
+	step: GiftFormStep
 
-	setGift: (gift: string) => void
-	setStep: (step: number) => void
+	setGift: (gift: GiftForm) => void
+	nextStep: () => void
+	previousStep: () => void
 }
 
-export const useGiftGeneratorFormStore = create<GiftGeneratorFormStore>()(set => ({
-	gift: "",
-	step: 0,
+export const useGiftGeneratorFormStore = create<GiftGeneratorFormStore>()((set, get) => ({
+	gift: null,
+	step: "genre",
 
 	setGift: gift => set({ gift }),
-	setStep: step => set({ step })
+	nextStep: () => {
+		const { step } = get()
+
+		if (step === "moreDetails") return
+		if (step === "genre") return set({ step: "ageRange" })
+		if (step === "ageRange") return set({ step: "event" })
+		if (step === "event") return set({ step: "hobbies" })
+		if (step === "hobbies") return set({ step: "moreDetails" })
+	},
+	previousStep: () => {
+		const { step } = get()
+
+		if (step === "genre") return
+		if (step === "ageRange") return set({ step: "genre" })
+		if (step === "event") return set({ step: "ageRange" })
+		if (step === "hobbies") return set({ step: "event" })
+		if (step === "moreDetails") return set({ step: "hobbies" })
+	}
 }))

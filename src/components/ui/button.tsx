8683821156f431a4a965d29a3aa074278848d8e4
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-11 13:16:54 +0200

    refactor: Update Button component to include onClick prop

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,10 +1,14 @@
 interface Props {
 	children: React.ReactNode
+	onClick: () => void
 }
 
-export default function Button({ children }: Props) {
+export default function Button({ children, onClick }: Props) {
 	return (
-		<button className="relative flex h-[30px] w-[90px] items-center justify-center gap-[10px] rounded-[10px] border-2 border-black bg-white p-[10px] drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] transition duration-200 hover:drop-shadow-[1px_1px_0px_rgba(0,0,0,1)]">
+		<button
+			className="relative flex h-[30px] w-[90px] items-center justify-center gap-[10px] rounded-[10px] border-2 border-black bg-white p-[10px] drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] transition duration-200 hover:drop-shadow-[1px_1px_0px_rgba(0,0,0,1)]"
+			onClick={onClick}
+		>
 			{children}
 		</button>
 	)

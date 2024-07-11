b37ab07f36816f22de019dde0077f73f765f0d66
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-11 13:04:58 +0200

    chore: Update Card component to include onClick prop

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,15 +3,17 @@ import { cn } from "@/utils/cn"
 interface Props {
 	children: React.ReactNode
 	className?: string
+	onClick?: () => void
 }
 
-export default function Card({ children, className }: Props) {
+export default function Card({ children, className, onClick }: Props) {
 	return (
 		<div
 			className={cn(
-				"min-w-[600px] rounded-[10px] border-[3px] border-black bg-white p-[10px] drop-shadow-[-3px_4px_0_rgba(0,0,0,1)]",
+				"rounded-[10px] border-[3px] border-black bg-white p-[10px] drop-shadow-[-3px_4px_0_rgba(0,0,0,1)]",
 				className
 			)}
+			onClick={onClick}
 		>
 			{children}
 		</div>

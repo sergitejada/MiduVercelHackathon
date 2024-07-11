07ecd0de5518988d7e3518aa6a613837f0d2c8cd
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-11 12:10:40 +0200

    chore: Add gift generator form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,22 @@
+import GiftGeneratorForm from "@/components/gift-generator-form/gift-generator-form"
 import { Logo } from "@/components/ui/logo"
-import Button from "../components/ui/button"
-import Card from "../components/ui/card"
 import { FlipWords } from "../components/ui/flip-words"
 
 const words = ["Padre", "Esposa", "Amigos", "Familia"]
+
 export default async function HomePage() {
 	return (
 		<div className="h-[500px] bg-orange-200">
 			<Logo alt="Logo" />
-			<div className="relative mx-auto flex max-w-7xl justify-center gap-10 pt-20">
+			<div className="relative mx-auto flex max-w-7xl justify-center gap-10 pt-16">
 				<div className="flex h-[40rem] items-center justify-center px-4">
-					<div className="mx-auto text-4xl font-bold">
+					<div className="mx-auto w-[550px] text-5xl font-semibold">
 						Sorprende a tu
 						<FlipWords words={words} /> <br />
 						con el Regalo Ideal
 					</div>
 				</div>
-
-				{/* <GiftForm /> */}
-				<Card>
-					<h1 className="text-white>Generador de text-6xl font-bold">AAAAAA</h1>
-					<div className="w-36">
-						<Button>Test</Button>
-					</div>
-				</Card>
+				<GiftGeneratorForm />
 			</div>
 		</div>
 	)
---
+++ src/components/gift-generator-form/gift-generator-form.tsx
@@ -0,0 +1,13 @@
+import Button from "@/components/ui/button"
+import Card from "@/components/ui/card"
+
+export default function GiftGeneratorForm() {
+	return (
+		<Card>
+			<h1 className="text-white>Generador de text-6xl font-bold">AAAAAA</h1>
+			<div className="w-36">
+				<Button>Test</Button>
+			</div>
+		</Card>
+	)
+}

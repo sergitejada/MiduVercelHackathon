6978b41cd60d1b8b5386c3ef7e099aa4b7796970
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-11 13:04:40 +0200

    chore: Update Card component to accept a custom className prop

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/utils/cn"
+
 interface Props {
 	children: React.ReactNode
+	className?: string
 }
 
-export default function Card({ children }: Props) {
+export default function Card({ children, className }: Props) {
 	return (
-		<div className="min-w-[600px] rounded-[10px] border-[3px] border-black bg-white p-[10px] drop-shadow-[-3px_4px_0_rgba(0,0,0,1)]">
+		<div
+			className={cn(
+				"min-w-[600px] rounded-[10px] border-[3px] border-black bg-white p-[10px] drop-shadow-[-3px_4px_0_rgba(0,0,0,1)]",
+				className
+			)}
+		>
 			{children}
 		</div>
 	)

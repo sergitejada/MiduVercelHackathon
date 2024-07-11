cf048465f0b7b0832272ecab20f6029d0862bd01
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-11 13:17:07 +0200

    feat: Improve gift generator form footer

--- src/components/gift-generator-form/gift-generator-form.tsx
+++ src/components/gift-generator-form/gift-generator-form.tsx
@@ -2,11 +2,15 @@
 
 import Button from "@/components/ui/button"
 import Card from "@/components/ui/card"
+import { useGiftGenerator } from "@/hooks/use-gift-generator"
 import { useGiftGeneratorFormStore } from "@/store/gift-generator-form-store"
 import GenreStep from "./genre-step/genre-step"
 
 export default function GiftGeneratorForm() {
 	const step = useGiftGeneratorFormStore(state => state.step)
+	const previousStep = useGiftGeneratorFormStore(state => state.previousStep)
+
+	const { hasPrevious } = useGiftGenerator()
 
 	return (
 		<Card className="flex min-w-[600px] cursor-pointer flex-col justify-between p-8">
@@ -14,9 +18,7 @@ export default function GiftGeneratorForm() {
 				<h1 className="text-white>Generador de text-6xl font-bold">AAAAAA</h1>
 			</header>
 			<div className="mt-8 grid flex-1 grid-cols-2 gap-4">{step === "genre" ? <GenreStep /> : <p>No step</p>}</div>
-			<footer className="flex justify-between">
-				<Button>Back</Button>
-			</footer>
+			<footer className="flex justify-between">{hasPrevious && <Button onClick={previousStep}>Back</Button>}</footer>
 			<div className="w-36" />
 		</Card>
 	)

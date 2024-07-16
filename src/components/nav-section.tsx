f7281fdbf80d303e7930b17920003ec6591d27df
Sergi Tejada <sergitejada0@gmail.com>
2024-07-16 15:22:20 +0200

    añadir urls

--- src/components/nav-section.tsx
+++ src/components/nav-section.tsx
@@ -4,11 +4,13 @@ import { Logo } from "./ui/logo"
 export default function NavSection() {
 	return (
 		<div className="mx-auto flex max-w-screen-2xl items-center justify-between p-4">
-			<Logo alt="Logo" />
+			<Link href="/">
+				<Logo alt="Logo" />
+			</Link>
 			<nav className="flex gap-8 underline">
 				<Link href="#como-funciona">Como Funciona</Link>
 				<Link href="#ideas-recientes">Ideas Recientes</Link>
-				<Link href="">Regalos para</Link>
+				<Link href="/regalos_para">Regalos para</Link>
 			</nav>
 		</div>
 	)

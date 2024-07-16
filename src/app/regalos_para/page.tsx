ec5983b2608e358e52566ea1a7f383415f1aed9f
Sergi Tejada <sergitejada0@gmail.com>
2024-07-16 15:47:11 +0200

    post list

--- src/app/regalos_para/page.tsx
+++ src/app/regalos_para/page.tsx
@@ -1,15 +1,27 @@
+import CardGift from "@/components/ui/card-gift"
+import { FlipWords } from "@/components/ui/flip-words"
+
+const words = ["Amigo", "Pareja", "Familia"]
 export default function RegalosParaPage() {
 	return (
 		<div className="flex flex-col">
-			<div className="n relative mx-auto flex max-w-7xl justify-center gap-10 pt-16">
-				<div className="mt-40 flex h-[40rem] flex-col justify-start gap-8 px-4">
-					<h1 className="text-4xl font-extrabold leading-[50px]">
-						Encuentra el Regalo Ideal <br />
-						para tu
-					</h1>
-					<h2 className="text-sm font-semibold text-gray-700">
-						Facilita la búsqueda de regalos con sugerencias personalizadas basadas en inteligencia artificial.
-					</h2>
+			<div className="relative mx-auto flex max-w-7xl justify-center gap-10 pt-16">
+				<div id="ideas-recientes" className="relative mx-auto flex max-w-7xl flex-col gap-10">
+					<div className="flex flex-col justify-start gap-4 px-4">
+						<h1 className="text-3xl font-extrabold">
+							Encuentra el Regalo para tu <FlipWords words={words} />
+						</h1>
+					</div>
+					<div className="flex flex-wrap justify-center gap-8">
+						<CardGift />
+						<CardGift />
+						<CardGift />
+						<CardGift />
+						<CardGift />
+						<CardGift />
+						<CardGift />
+						<CardGift />
+					</div>
 				</div>
 			</div>
 		</div>

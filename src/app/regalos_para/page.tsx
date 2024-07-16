1c2dbff7cf3a4932161c378ebdf5c17f53eee3e7
Sergi Tejada <sergitejada0@gmail.com>
2024-07-16 15:21:52 +0200

    neuva url

---
+++ src/app/regalos_para/page.tsx
@@ -0,0 +1,17 @@
+export default function RegalosParaPage() {
+	return (
+		<div className="flex flex-col">
+			<div className="n relative mx-auto flex max-w-7xl justify-center gap-10 pt-16">
+				<div className="mt-40 flex h-[40rem] flex-col justify-start gap-8 px-4">
+					<h1 className="text-4xl font-extrabold leading-[50px]">
+						Encuentra el Regalo Ideal <br />
+						para tu
+					</h1>
+					<h2 className="text-sm font-semibold text-gray-700">
+						Facilita la búsqueda de regalos con sugerencias personalizadas basadas en inteligencia artificial.
+					</h2>
+				</div>
+			</div>
+		</div>
+	)
+}

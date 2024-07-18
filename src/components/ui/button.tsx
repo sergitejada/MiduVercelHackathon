dd66f7de127143800c5c3e98479d33dc96ade0ea
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-18 18:59:44 +0200

    refactor: Update Button component to accept a className prop and support disabled state

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,13 +1,21 @@
+import { cn } from "@/lib/utils"
+
 interface Props {
 	children: React.ReactNode
-	onClick: () => void
+	className?: string
+	disabled: boolean
+	onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-export default function Button({ children, onClick }: Props) {
+export default function Button({ children, className, disabled, onClick }: Props) {
 	return (
 		<button
-			className="relative flex h-[30px] w-[90px] items-center justify-center gap-[10px] rounded-[10px] border-2 border-black bg-white p-[10px] drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] transition duration-200 hover:drop-shadow-[1px_1px_0px_rgba(0,0,0,1)]"
+			className={cn(
+				"relative flex items-center justify-center gap-[10px] rounded-[10px] border-2 border-black bg-white px-4 py-1 drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] transition duration-200 hover:drop-shadow-[1px_1px_0px_rgba(0,0,0,1)] disabled:cursor-not-allowed disabled:opacity-50 disabled:drop-shadow-none disabled:transition-none disabled:hover:drop-shadow-none",
+				className
+			)}
 			onClick={onClick}
+			disabled={disabled}
 		>
 			{children}
 		</button>

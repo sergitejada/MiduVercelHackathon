621205ea4f4f56c9d6ca1876c6aaad38d29fbea9
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-23 01:41:16 +0200

    chore: Refactor Textarea component to support default value and forwardRef

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,22 +1,31 @@
 import { cn } from "@/lib/utils"
+import { forwardRef } from "react"
 
 interface Props {
 	placeholder?: string
 	className?: string
 	rows?: number
 	maxLength?: number
+	defaultValue?: string
 }
 
-export default function Textarea({ placeholder, className, maxLength, rows }: Props) {
+export const Textarea = forwardRef(function Textarea(
+	{ placeholder, className, maxLength, rows, defaultValue }: Props,
+	ref: React.ForwardedRef<HTMLTextAreaElement>
+) {
 	return (
 		<textarea
+			ref={ref}
 			className={cn(
 				"relative flex items-center justify-center gap-[10px] rounded-[10px] border-[2px] border-black bg-white p-[10px] drop-shadow-[-2px_2px_0px_rgba(0,0,0,1)] transition duration-200 placeholder:text-neutral-400 focus-visible:outline-none",
 				className
 			)}
+			defaultValue={defaultValue ?? ""}
 			maxLength={maxLength ?? 3000}
 			placeholder={placeholder}
 			rows={rows ?? 5}
 		/>
 	)
-}
+})
+
+export default Textarea

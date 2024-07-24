bce917b1d06743694dc626cfb2866f0cc8dd62eb
Sergi Tejada <sergitejada0@gmail.com>
2024-07-24 12:42:07 +0200

    add btn

--- src/components/nav-section.tsx
+++ src/components/nav-section.tsx
@@ -4,23 +4,20 @@ import { Logo } from "./ui/logo"
 export default function NavSection() {
 	return (
 		<div className="mx-auto flex max-w-screen-2xl items-center justify-between p-4">
-			<Link href="/" className="flex items-center gap-4">
+			<Link href="/" className="flex items-center gap-2">
 				<Logo alt="Logo" />
-				<span className="">GiftHunter</span>
+				<span className="text-xl font-bold">GIFTHUNTER</span>
 			</Link>
 
-			<nav className="flex gap-8">
-				<Link className="hover:underline" href="/">
-					Generar Ideas
-				</Link>
-				<Link className="hover:underline" href="/#como-funciona">
-					Como Funciona
-				</Link>
+			<nav className="flex items-center gap-8">
 				<Link className="hover:underline" href="/#ideas-recientes">
 					Ideas Recientes
 				</Link>
 				<Link className="hover:underline" href="/blog">
 					Blog
+				</Link>
+				<Link className="rounded-xl border-2 border-black bg-white px-1.5 py-1 hover:bg-orange-50" href="/">
+					Generar Ideas
 				</Link>
 			</nav>
 		</div>

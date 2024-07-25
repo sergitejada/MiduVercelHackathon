6071974bc98947d2777935bac6bd31babdb0e3f4
Sergi Tejada <sergitejada0@gmail.com>
2024-07-25 11:51:22 +0200

    btn solo si se hace scroll

--- src/components/back-to-top.tsx
+++ src/components/back-to-top.tsx
@@ -1,29 +1,50 @@
 "use client"
 
+import { useEffect, useState } from "react"
+
 export default function BackToTop() {
+	const [isVisible, setIsVisible] = useState(false)
 	function handleBackToTop() {
 		window.scrollTo({
 			top: 0,
 			behavior: "smooth"
 		})
 	}
+	useEffect(() => {
+		function toggleVisibility() {
+			if (window.pageYOffset > 300) {
+				// Cambia este valor según cuándo quieres que aparezca el botón
+				setIsVisible(true)
+			} else {
+				setIsVisible(false)
+			}
+		}
+
+		window.addEventListener("scroll", toggleVisibility)
+
+		return () => {
+			window.removeEventListener("scroll", toggleVisibility)
+		}
+	}, [])
 
 	return (
 		<div>
-			<button
-				className="fixed bottom-8 right-8 rounded-full border-4 border-black bg-white p-4 text-black drop-shadow-[3px_4px_0_rgba(0,0,0,1)]"
-				onClick={handleBackToTop}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					className="h-6 w-6"
-					fill="currentColor"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
+			{isVisible && (
+				<button
+					className="fixed bottom-8 right-8 animate-bounce rounded-full border-4 border-black bg-white p-4 text-black drop-shadow-[3px_4px_0_rgba(0,0,0,1)]"
+					onClick={handleBackToTop}
 				>
-					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={4} d="M5 10l7-7m0 0l7 7m-7-7v18" />
-				</svg>
-			</button>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-6 w-6"
+						fill="currentColor"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={4} d="M5 10l7-7m0 0l7 7m-7-7v18" />
+					</svg>
+				</button>
+			)}
 		</div>
 	)
 }

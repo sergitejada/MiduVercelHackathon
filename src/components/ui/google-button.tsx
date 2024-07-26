bb18983129c082d8f3f4749fb2e0eab4a5d8695f
Sergi Tejada <sergitejada0@gmail.com>
2024-07-26 13:59:32 +0200

    btc con solo logo

--- src/components/ui/google-button.tsx
+++ src/components/ui/google-button.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image"
 
-export default function GoogleButton() {
+interface Props {
+	only_logo?: boolean
+}
+
+export default function GoogleButton({ only_logo = false }: Props) {
 	return (
-		<div className="flex items-center justify-center dark:bg-gray-800">
+		<div className="flex w-full items-center justify-center">
 			<button className="flex gap-2 rounded-lg border border-slate-200 px-4 py-2 text-slate-700 transition duration-150 hover:border-slate-400 hover:text-slate-900 hover:shadow dark:border-slate-700 dark:text-slate-200 dark:hover:border-slate-500 dark:hover:text-slate-300">
 				<Image
 					className="h-6 w-6"
@@ -12,7 +16,7 @@ export default function GoogleButton() {
 					width={24}
 					height={24}
 				/>
-				<span>Ver en Google</span>
+				{!only_logo && <span>Ver en Google</span>}
 			</button>
 		</div>
 	)

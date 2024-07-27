38a40d9a839c36f1ee030a6823f804b45ce39e82
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-27 13:53:20 +0200

    feat: Update generate-gifts route to return JSON response

--- src/app/api/generate-gifts/route.ts
+++ src/app/api/generate-gifts/route.ts
@@ -4,15 +4,20 @@ import { generateText } from "ai"
 export async function POST(req: Request) {
 	const { prompt } = await req.json()
 
+	console.log("hi")
+	console.log(prompt)
+
 	try {
 		const { text } = await generateText({
 			model: openai("gpt-3.5-turbo"),
 			prompt
 		})
 
-		return new Response(text, { status: 200 })
+		console.log(text)
+
+		return Response.json(text, { status: 200 })
 	} catch (e) {
 		console.error(e)
-		return new Response(`Error: ${e}`, { status: 400 })
+		return Response.json(`Error: ${e}`, { status: 400 })
 	}
 }

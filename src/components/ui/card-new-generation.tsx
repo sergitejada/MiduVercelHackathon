8036e4b846d1d9bdb06fd6dfefdc57fda61a8d30
Sergi Tejada <sergitejada0@gmail.com>
2024-07-27 14:45:42 +0200

    mas pequeño

--- src/components/ui/card-new-generation.tsx
+++ src/components/ui/card-new-generation.tsx
@@ -27,21 +27,21 @@ export default function CardNewGeneration({ name, description }: Props) {
 				<Card
 					onClick={handleFlipCard}
 					className={cn(
-						"flex h-[30rem] cursor-pointer flex-col justify-between rounded-[10px] border-[3px] border-black bg-white p-[10px] drop-shadow-[-3px_4px_0_rgba(0,0,0,1)]",
+						"flex h-[28rem] cursor-pointer flex-col justify-between rounded-[10px] border-[3px] border-black bg-white p-[10px] drop-shadow-[-3px_4px_0_rgba(0,0,0,1)]",
 						!isFlipped && "transition-transform hover:scale-105"
 					)}
 				>
 					<Image src="/assets/present.svg" alt="Gift" width={350} height={350} className="mx-auto rounded" />
 					<p className="text-center">CLICK PARA REVELAR</p>
 				</Card>
-				<Card className="flex h-[30rem] flex-col gap-8 transition-none">
+				<Card className="flex h-[28rem] flex-col gap-8 transition-none">
 					<Image src="/assets/royal-oak.jpg" alt="Image" width={350} height={350} className="rounded" />
 					<div className="flex flex-col gap-2">
 						<h3 className="font-semibold">{name}</h3>
 						<p className="text-xs text-gray-600">{description}</p>
 					</div>
-					<div className="flex gap-2">
-						<GoogleButton onlyLogo={true} />
+					<div className="flex justify-start gap-2">
+						<GoogleButton onlyLogo={true} url={name} />
 						<AmazonButton onlyLogo />
 					</div>
 				</Card>

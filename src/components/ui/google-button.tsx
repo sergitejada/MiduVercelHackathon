41880188281e8aeffad1976c761a01fa3c4a3bee
Sergi Tejada <sergitejada0@gmail.com>
2024-07-27 14:45:50 +0200

    permitir link al btn

--- src/components/ui/google-button.tsx
+++ src/components/ui/google-button.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image"
+import Link from "next/link"
 
 interface Props {
 	onlyLogo?: boolean
+	url: string
 }
 
-export default function GoogleButton({ onlyLogo = false }: Props) {
+export default function GoogleButton({ onlyLogo = false, url }: Props) {
 	return (
 		<div className="flex w-full items-center justify-center">
-			<button className="flex gap-2 rounded-lg border border-slate-200 px-4 py-2 text-slate-700 transition duration-150 hover:border-slate-400 hover:text-slate-900 hover:shadow dark:border-slate-700 dark:text-slate-200 dark:hover:border-slate-500 dark:hover:text-slate-300">
-				<Image
-					className="h-6 w-6"
-					src="/assets/google-logo.svg"
-					loading="lazy"
-					alt="google logo"
-					width={24}
-					height={24}
-				/>
-				{!onlyLogo && <span>Ver en Google</span>}
-			</button>
+			<Link href={`https://www.google.com/search?q=${url}`} target="_blank">
+				<button className="flex gap-2 rounded-lg border border-slate-200 px-4 py-2 text-slate-700 transition duration-150 hover:border-slate-400 hover:text-slate-900 hover:shadow">
+					<Image
+						className="h-6 w-6"
+						src="/assets/google-logo.svg"
+						loading="lazy"
+						alt="google logo"
+						width={24}
+						height={24}
+					/>
+					{!onlyLogo && <span>Ver en Google</span>}
+				</button>
+			</Link>
 		</div>
 	)
 }

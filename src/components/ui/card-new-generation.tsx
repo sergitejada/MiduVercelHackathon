206211b98c7ab0de97224d6a8057914f547cc17a
Sergi Tejada <sergitejada0@gmail.com>
2024-07-28 20:02:24 +0200

    card de productos

--- src/components/ui/card-new-generation.tsx
+++ src/components/ui/card-new-generation.tsx
@@ -27,18 +27,20 @@ export default function CardNewGeneration({ name, description }: Props) {
 				<Card
 					onClick={handleFlipCard}
 					className={cn(
-						"flex h-[28rem] cursor-pointer flex-col justify-between rounded-[10px] border-[3px] border-black bg-white p-[10px] drop-shadow-[-3px_4px_0_rgba(0,0,0,1)]",
+						"flex h-[28rem] w-full max-w-80 cursor-pointer flex-col justify-between rounded-[10px] border-[3px] border-black bg-white p-[10px] drop-shadow-[-3px_4px_0_rgba(0,0,0,1)]",
 						!isFlipped && "transition-transform hover:scale-105"
 					)}
 				>
 					<Image src="/assets/present.svg" alt="Gift" width={350} height={350} className="mx-auto rounded" />
 					<p className="text-center">CLICK PARA REVELAR</p>
 				</Card>
-				<Card className="flex h-[28rem] flex-col gap-8 transition-none">
-					<Image src="/assets/royal-oak.jpg" alt="Image" width={350} height={350} className="rounded" />
-					<div className="flex flex-col gap-2">
-						<h3 className="font-semibold">{name}</h3>
-						<p className="text-xs text-gray-600">{description}</p>
+				<Card className="flex h-[28rem] w-full max-w-80 flex-col justify-between gap-8 transition-none">
+					<div className="flex flex-col gap-8">
+						<Image src="/assets/royal-oak.jpg" alt="Image" width={350} height={350} className="rounded" />
+						<div className="flex flex-col gap-2">
+							<h3 className="font-semibold">{name}</h3>
+							<p className="text-xs text-gray-600">{description}</p>
+						</div>
 					</div>
 					<div className="flex justify-start gap-2">
 						<GoogleButton onlyLogo={true} url={name} />

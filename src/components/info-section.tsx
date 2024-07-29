28ede9470e8beedbe40cbfe642bc4e3569fd4a93
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-29 20:37:36 +0200

    refactor: Update InfoSection component to use dynamic data

--- src/components/info-section.tsx
+++ src/components/info-section.tsx
@@ -16,10 +16,10 @@ const ITEMS = [
 
 export default function InfoSection() {
 	return (
-		<section className="bg-gradient-to-b from-neutral-100 to-neutral-50 py-32">
+		<section className="py-12">
 			<div className="container mx-auto grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
 				{ITEMS.map(item => (
-					<article key={item.title} className="rounded-lg bg-white p-6 shadow-md">
+					<article key={item.title} className="rounded-lg border bg-white p-6 shadow-md">
 						<h2 className="mb-4 text-2xl font-semibold text-orange-500">{item.title}</h2>
 						<p className="text-gray-700">{item.description}</p>
 					</article>

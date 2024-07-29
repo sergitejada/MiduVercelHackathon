ef969a304d7f3ac7d322c7ac1f45caad0076fcb4
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-29 20:16:04 +0200

    refactor: Update InfoSection component to use dynamic data

--- src/components/info-section.tsx
+++ src/components/info-section.tsx
@@ -1,26 +1,30 @@
+const ITEMS = [
+	{
+		title: "Personaliza Cada Ocasión",
+		description:
+			"Nuestra IA analiza la información que proporcionas para encontrar el regalo ideal para cualquier evento."
+	},
+	{
+		title: "Ahorra Tiempo y Esfuerzo",
+		description: "Olvídate de buscar durante horas. Deja que nuestra tecnología avanzada haga el trabajo por ti."
+	},
+	{
+		title: "Fácil y Rápido de Usar",
+		description: "Introduce algunos detalles y nuestra IA se encargará del resto. ¡Así de simple!"
+	}
+]
+
 export default function InfoSection() {
 	return (
-		<div className="bg-gradient-to-b from-neutral-100 to-neutral-50 py-32">
-			<div className="mx-auto grid max-w-7xl grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-				<div className="rounded-lg bg-white p-6 shadow-md">
-					<h2 className="mb-2 text-2xl font-semibold text-orange-500">Personaliza Cada Ocasión</h2>
-					<p className="text-gray-700">
-						Nuestra IA analiza la información que proporcionas para encontrar el regalo ideal para cualquier evento.
-					</p>
-				</div>
-				<div className="rounded-lg bg-white p-6 shadow-md">
-					<h2 className="mb-2 text-2xl font-semibold text-orange-500">Ahorra Tiempo y Esfuerzo</h2>
-					<p className="text-gray-700">
-						Olvídate de buscar durante horas. Deja que nuestra tecnología avanzada haga el trabajo por ti.
-					</p>
-				</div>
-				<div className="rounded-lg bg-white p-6 shadow-md">
-					<h2 className="mb-2 text-2xl font-semibold text-orange-500">Fácil y Rápido de Usar</h2>
-					<p className="text-gray-700">
-						Introduce algunos detalles y nuestra IA se encargará del resto. ¡Así de simple!
-					</p>
-				</div>
+		<section className="bg-gradient-to-b from-neutral-100 to-neutral-50 py-32">
+			<div className="container mx-auto grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+				{ITEMS.map(item => (
+					<article key={item.title} className="rounded-lg bg-white p-6 shadow-md">
+						<h2 className="mb-4 text-2xl font-semibold text-orange-500">{item.title}</h2>
+						<p className="text-gray-700">{item.description}</p>
+					</article>
+				))}
 			</div>
-		</div>
+		</section>
 	)
 }

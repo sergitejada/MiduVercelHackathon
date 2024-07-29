fc277fa15e30bd92381969424ef3d7f383e0f1e8
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-29 19:59:25 +0200

    refactor: Regenerate gifts on generate more gifts button

--- src/components/form-generator-results-section/form-generator-results-section.tsx
+++ src/components/form-generator-results-section/form-generator-results-section.tsx
@@ -1,12 +1,17 @@
 "use client"
 
 import { useCleanResults } from "@/hooks/use-clean-results"
+import { useGenerateGifts } from "@/hooks/use-generate-gifts"
+import { useGiftGeneratorFormStore } from "@/store/gift-generator-form-store"
 import { useEffect, useRef } from "react"
 import Button from "../ui/button"
 import CardNewGeneration from "../ui/card-new-generation"
 
 export default function FormGeneratorResultsSection() {
+	const gift = useGiftGeneratorFormStore(state => state.gift)
+
 	const { status, results } = useCleanResults()
+	const { generateResults } = useGenerateGifts()
 
 	const formGeneratorResultsRef = useRef<HTMLDetailsElement>(null)
 
@@ -18,6 +23,13 @@ export default function FormGeneratorResultsSection() {
 
 		element.scrollIntoView({ behavior: "smooth", block: "center" })
 	}, [status, formGeneratorResultsRef])
+
+	async function handleGenerateMoreGifts() {
+		if (!gift) return
+
+		// !TODO: Maybe send the existing gifts to the server to avoid duplicates
+		await generateResults(gift)
+	}
 
 	if (status === "idle") return null
 
@@ -36,7 +48,7 @@ export default function FormGeneratorResultsSection() {
 								))}
 							</div>
 							<div className="mx-auto mt-8">
-								<Button>Generar mas regalos</Button>
+								<Button onClick={handleGenerateMoreGifts}>Generar mas regalos</Button>
 							</div>
 						</>
 					)}

d09a2f3d8b81d2f940a27bbfb4c22645280269d3
Sergi Tejada <sergitejada0@gmail.com>
2024-07-29 12:18:42 +0200

    modiifcar response por version de ts

--- src/app/api/generate-gifts/route.ts
+++ src/app/api/generate-gifts/route.ts
@@ -1,5 +1,6 @@
 import { openai } from "@ai-sdk/openai"
 import { generateText } from "ai"
+import { NextResponse } from "next/server"
 
 const context =
 	"El usuario te proporcionara 7 características te las adjunto a continuación entre =={característica}==, el usuario hará lo mismo\n-Genero-,-Edad-,-Hobby-,-Evento-,-Tipo de persona-,-Presupuesto-,-Descripción-\n\nCon las características proporcionadas recomienda regalos acorde a la persona\n\nLa salida siempre serán 4 recomendaciones ';{articulo}:{descripción};'\nNo añadas ningún texto de introducción al principio de la respuesta, siempre devuelve las recomendaciones directamente\nNo añadas en ningún momento el precio \nLa respuesta siempre será el nombre del articulo y una pequeña descripción máximo 200 caracteres para la descripción\nNo numeres los resultados, no le pongas ningún símbolo en la parte inicial\nNunca le digas al usuario que necesitas mas información\nLa salida siempre será en castellano"
@@ -18,9 +19,9 @@ export async function POST(req: Request) {
 
 		console.log(text)
 
-		return Response.json(text, { status: 200 })
+		return NextResponse.json(text, { status: 200 })
 	} catch (e) {
 		console.error(e)
-		return Response.json(`Error: ${e}`, { status: 400 })
+		return NextResponse.json(`Error: ${e}`, { status: 400 })
 	}
 }

c6fe264d0d860098a2ef4dd8d4eeb8a41e1ba168
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-30 23:36:11 +0200

    feat: Add amazon external images host to config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
 	reactStrictMode: false,
 	experimental: {
 		serverComponentsExternalPackages: ["paapi5-nodejs-sdk"]
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "m.media-amazon.com"
+			}
+		]
 	}
 }
 

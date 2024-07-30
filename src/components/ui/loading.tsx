f7b729f9a3a2bc0bb640f89c52429db105f70097
RubenGlam <rubenbermudezsaez@gmail.com>
2024-07-30 23:52:06 +0200

    refactor: Update loading component with optimized SVG animation

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -1,58 +1,39 @@
 export default function Loading() {
 	return (
-		<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill="#000">
-			<circle
-				cx="50"
-				cy="50"
-				r="40"
-				stroke="#000"
-				strokeWidth="5"
-				fill="none"
-				strokeDasharray="251.2"
-				strokeDashoffset="251.2"
-			>
-				<animate attributeName="stroke-dashoffset" from="251.2" to="0" dur="2s" repeatCount="indefinite" />
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="100" height="100">
+			<circle fill="#000000" stroke="#000000" strokeWidth="15" r="15" cx="40" cy="65">
+				<animate
+					attributeName="cy"
+					calcMode="spline"
+					dur="2"
+					values="65;135;65;"
+					keySplines=".5 0 .5 1;.5 0 .5 1"
+					repeatCount="indefinite"
+					begin="-.4"
+				/>
 			</circle>
-			<line x1="50" y1="10" x2="50" y2="20" stroke="#000" strokeWidth="5">
-				<animateTransform
-					attributeName="transform"
-					type="rotate"
-					from="0 50 50"
-					to="360 50 50"
-					dur="2s"
+			<circle fill="#000000" stroke="#000000" strokeWidth="15" r="15" cx="100" cy="65">
+				<animate
+					attributeName="cy"
+					calcMode="spline"
+					dur="2"
+					values="65;135;65;"
+					keySplines=".5 0 .5 1;.5 0 .5 1"
 					repeatCount="indefinite"
+					begin="-.2"
 				/>
-			</line>
-			<line x1="50" y1="90" x2="50" y2="80" stroke="#000" strokeWidth="5">
-				<animateTransform
-					attributeName="transform"
-					type="rotate"
-					from="0 50 50"
-					to="360 50 50"
-					dur="2s"
+			</circle>
+			<circle fill="#000000" stroke="#000000" strokeWidth="15" r="15" cx="160" cy="65">
+				<animate
+					attributeName="cy"
+					calcMode="spline"
+					dur="2"
+					values="65;135;65;"
+					keySplines=".5 0 .5 1;.5 0 .5 1"
 					repeatCount="indefinite"
+					begin="0"
 				/>
-			</line>
-			<line x1="10" y1="50" x2="20" y2="50" stroke="#000" strokeWidth="5">
-				<animateTransform
-					attributeName="transform"
-					type="rotate"
-					from="0 50 50"
-					to="360 50 50"
-					dur="2s"
-					repeatCount="indefinite"
-				/>
-			</line>
-			<line x1="90" y1="50" x2="80" y2="50" stroke="#000" strokeWidth="5">
-				<animateTransform
-					attributeName="transform"
-					type="rotate"
-					from="0 50 50"
-					to="360 50 50"
-					dur="2s"
-					repeatCount="indefinite"
-				/>
-			</line>
+			</circle>
 		</svg>
 	)
 }

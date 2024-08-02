6dfae2e8687df1d986cb23d298bf6a0fb261df33
Sergi Tejada <sergitejada0@gmail.com>
2024-08-02 10:46:55 +0200

    card product

--- src/components/ui/card-product.tsx
+++ src/components/ui/card-product.tsx
@@ -1,28 +1,34 @@
-import Image from "next/image"
-import AmazonButton from "./amazon-button"
-import GoogleButton from "./google-button"
+import Image from "next/image";
+import AmazonButton from "./amazon-button";
+import GoogleButton from "./google-button";
 
 interface Props {
-	src: string
-	title: string
-	description: string
-	link: string
+  src: string;
+  title: string;
+  description: string;
+  link: string;
 }
 
 export default function CardProduct({ src, title, description, link }: Props) {
-	return (
-		<div className="mt-4 flex gap-8 border p-1">
-			<div>
-				<Image src={src} alt={""} width={150} height={150} className="h-[150px] w-[150px] rounded object-cover" />
-			</div>
-			<div className="mt-6 flex flex-col gap-4">
-				<h4>{title}</h4>
-				<div className="flex flex-row gap-4">
-					<GoogleButton url="" />
-					<AmazonButton />
-					<AmazonButton />
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="mt-4 flex gap-8 border p-1">
+      <div>
+        <Image
+          src={src}
+          alt={""}
+          width={150}
+          height={150}
+          className="h-[150px] w-[150px] rounded object-cover"
+        />
+      </div>
+      <div className="mt-6 flex w-3/4 flex-col gap-4">
+        <h4 className="text-center text-xl">{title}</h4>
+        <div className="flex w-auto flex-row justify-start gap-4">
+          <GoogleButton url={title} />
+          <AmazonButton />
+          {/* <AmazonButton /> */}
+        </div>
+      </div>
+    </div>
+  );
 }

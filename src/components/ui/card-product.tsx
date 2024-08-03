4380a9a063a87abcd324243882f31d9e14ccf585
RubenGlam <rubenbermudezsaez@gmail.com>
2024-08-03 05:49:30 +0200

    refactor: Update CardProduct component to use text-start class for title alignment

--- src/components/ui/card-product.tsx
+++ src/components/ui/card-product.tsx
@@ -16,7 +16,7 @@ export default function CardProduct({ src, title, description, link }: Props) {
 				<Image src={src} alt={title} width={150} height={150} className="aspect-square rounded object-cover" />
 			</div>
 			<div className="mt-6 flex w-3/4 flex-col gap-4">
-				<h4 className="text-center text-xl">{title}</h4>
+				<h4 className="text-start text-xl">{title}</h4>
 				<div className="flex w-auto flex-row justify-start gap-4">
 					<GoogleButton url={title} />
 					<AmazonButton search={title} />

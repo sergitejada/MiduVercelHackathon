609f2fb4d6b64c4b5ac8efc0ae3e718d4be4e5d0
Sergi Tejada <sergitejada0@gmail.com>
2024-08-03 19:35:42 +0200

    fetch other gifts

--- src/components/new-generations-section/new-generations-section.tsx
+++ src/components/new-generations-section/new-generations-section.tsx
@@ -1,23 +1,29 @@
-"use client"
-
+import { Key } from "react"
 import CardNewGeneration from "../ui/card-new-generation"
 
-export default function NewGenerationsSection() {
-	// const amazonApi = useAmazonApi()
-	// console.log(amazonApi)
+export default async function NewGenerationsSection() {
+	const response = await fetch("http://localhost:3000/api/db")
+	const data = await response.json()
+
 	return (
 		<section id="ideas-recientes" className="relative py-20">
 			<div className="container mx-auto flex w-full flex-col gap-20">
 				<h2 className="text-center text-3xl font-semibold">Ideas Recientes</h2>
 				<div className="grid justify-center gap-12 md:grid-cols-2 lg:grid-cols-4">
-					<CardNewGeneration
-						name="Basketball Hoop Set"
-						description="Adjustable basketball hoop with a sturdy steel frame and shatterproof backboard. Perfect for indoor or outdoor play."
-						isFlipped
-					/>
-					<CardNewGeneration name="test" isFlipped />
-					<CardNewGeneration name="test" isFlipped />
-					<CardNewGeneration name="test" isFlipped />
+					{data.map(
+						(
+							item: { name: string; description: string | undefined; images: string },
+							index: Key | null | undefined
+						) => (
+							<CardNewGeneration
+								key={index}
+								name={item.name}
+								description={item.description}
+								img={item.images}
+								isFlipped
+							/>
+						)
+					)}
 				</div>
 			</div>
 		</section>

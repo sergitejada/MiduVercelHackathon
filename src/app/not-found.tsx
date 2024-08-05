ffe8a263e76788e927654960c74cc27befeb5173
RubenGlam <rubenbermudezsaez@gmail.com>
2024-08-05 18:56:43 +0200

    refactor: Add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+		<div className="my-24 flex flex-col gap-8 bg-orange-200 text-center">
+			<h1 className="text-8xl font-semibold">404</h1>
+			<p className="text-2xl">Página no encontrada</p>
+		</div>
+	)
+}

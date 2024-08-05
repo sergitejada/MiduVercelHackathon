af0baceff7820ac07d72e922ac4a4258d6b3049c
RubenGlam <rubenbermudezsaez@gmail.com>
2024-08-05 18:58:15 +0200

    refactor: Update NotFound component to include link to homepage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link"
+
 export default function NotFound() {
 	return (
-		<div className="my-24 flex flex-col gap-8 bg-orange-200 text-center">
-			<h1 className="text-8xl font-semibold">404</h1>
-			<p className="text-2xl">Página no encontrada</p>
+		<div className="bg-orange-200">
+			<div className="my-24 flex flex-col gap-8 text-center">
+				<h1 className="text-8xl font-semibold">404</h1>
+				<p className="text-2xl">Página no encontrada</p>
+				<Link href="/">
+					<button className="mx-auto rounded-md bg-orange-400 p-2 px-4 text-white">Volver al inicio</button>
+				</Link>
+			</div>
 		</div>
 	)
 }

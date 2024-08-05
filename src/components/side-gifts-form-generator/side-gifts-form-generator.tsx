718c85b78b9d5d259487453c53a570de36d69714
RubenGlam <rubenbermudezsaez@gmail.com>
2024-08-05 18:53:04 +0200

    refactor: Side form

--- src/components/side-gifts-form-generator/side-gifts-form-generator.tsx
+++ src/components/side-gifts-form-generator/side-gifts-form-generator.tsx
@@ -1,64 +1,174 @@
+"use client"
+
+import { useGenerateGifts } from "@/hooks/use-generate-gifts"
+import { useGiftGeneratorFormStore } from "@/store/gift-generator-form-store"
+import { AgeRange, Event, Genre, GiftForm, Hobbies, Relation } from "@/types/types"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 import Button from "../ui/button"
 
+const genres: { label: string; value: Genre }[] = [
+	{ label: "Chico", value: "man" },
+	{
+		label: "Chica",
+		value: "women"
+	}
+]
+
+const relations: { label: string; value: Relation }[] = [
+	{ label: "Pareja", value: "couple" },
+	{ label: "Compañera de trabajo", value: "coworker" },
+	{ label: "Amiga", value: "friend" },
+	{ label: "Abuela", value: "grandfather" },
+	{ label: "Nieta", value: "grandson" },
+	{ label: "Hija", value: "son" }
+]
+
+const events: { label: string; value: Event }[] = [
+	{ label: "Cumpleaños", value: "birthday" },
+	{ label: "Navidad", value: "christmas" },
+	{ label: "San Valentín", value: "valentine" },
+	{ label: "Graduación", value: "graduation" }
+]
+
+const hobbies: { label: string; value: Hobbies }[] = [
+	{ label: "Música", value: "music" },
+	{ label: "Deporte", value: "sports" },
+	{ label: "Lectura", value: "reading" },
+	{ label: "Cocina", value: "cooking" },
+	{ label: "Viajar", value: "travel" },
+	{ label: "Comer", value: "eat" }
+]
+
+const ages: { label: string; value: AgeRange }[] = [
+	{ label: "0-12", value: "0-12" },
+	{ label: "13-18", value: "13-18" },
+	{ label: "19-30", value: "19-30" },
+	{ label: "31-50", value: "31-50" },
+	{ label: "51-70", value: "51-70" },
+	{ label: ">70", value: ">70" }
+]
+
 export default function SideGiftsFormGenerator() {
+	const setGift = useGiftGeneratorFormStore(state => state.setGift)
+	const clearResults = useGiftGeneratorFormStore(state => state.clearResults)
+	const results = useGiftGeneratorFormStore(state => state.results)
+	const giftGenerationStatus = useGiftGeneratorFormStore(state => state.giftGenerationStatus)
+
+	const { generateResults } = useGenerateGifts()
+	const router = useRouter()
+
+	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault()
+		const formData = new FormData(e.currentTarget)
+
+		const hobbies = formData.get("hobbies") as Hobbies
+
+		const newGift: GiftForm = {
+			ageRange: formData.get("ageRange") as AgeRange,
+			event: formData.get("events") as Event,
+			genre: formData.get("genre") as Genre,
+			hobbies: [hobbies] as Hobbies[],
+			relation: formData.get("relations") as Relation,
+			moreDetails: formData.get("moreDetails") as string
+		}
+
+		setGift({
+			...newGift
+		})
+
+		clearResults()
+
+		await generateResults(newGift)
+	}
+
+	useEffect(() => {
+		if (giftGenerationStatus === "error") {
+			alert("Error generando regalos")
+		}
+
+		if (results) {
+			router.push("/")
+		}
+	}, [giftGenerationStatus, results])
+
 	return (
 		<aside className="h-fit min-w-[25rem] rounded-md bg-white drop-shadow-lg">
 			<header className="rounded-t-md bg-orange-400 p-4">
 				<h2 className="text-xl font-semibold text-white">Encuentra el regalo perfecto</h2>
 			</header>
 			<div className="p-4">
-				<form>
+				{giftGenerationStatus === "generating" && <p className="py-4 text-center">Generando regalos...</p>}
+				{giftGenerationStatus === "error" && <p className="py-4 text-center">Error generando regalos</p>}
+				<form onSubmit={handleSubmit}>
 					<div className="mb-4">
-						<label htmlFor="recipient" className="block text-sm font-semibold text-gray-600">
+						<label htmlFor="genre" className="block text-sm font-semibold text-gray-600">
 							Para
 						</label>
-						<select id="recipient" name="recipient" className="mt-1 w-full rounded-md border p-2">
-							<option value="1">Hombre</option>
-							<option value="2">Mujer</option>
-							<option value="3">Niño</option>
-							<option value="4">Niña</option>
+						<select id="genre" name="genre" className="mt-1 w-full rounded-md border p-2">
+							{genres.map(({ label, value }) => (
+								<option key={value} value={value}>
+									{label}
+								</option>
+							))}
 						</select>
 					</div>
 					<div className="mb-4">
-						<label htmlFor="price" className="block text-sm font-semibold text-gray-600">
-							Precio
+						<label htmlFor="relations" className="block text-sm font-semibold text-gray-600">
+							Qué es
 						</label>
-						<select id="price" name="price" className="mt-1 w-full rounded-md border p-2 focus:outline-none">
-							<option value="1">Menos de $50</option>
-							<option value="2">$50 - $100</option>
-							<option value="3">$100 - $200</option>
-							<option value="4">Más de $200</option>
+						<select id="relations" name="relations" className="mt-1 w-full rounded-md border p-2">
+							{relations.map(({ label, value }) => (
+								<option key={value} value={value}>
+									{label}
+								</option>
+							))}
 						</select>
 					</div>
 					<div className="mb-4">
-						<label htmlFor="occasion" className="block text-sm font-semibold text-gray-600">
-							Ocasión
+						<label htmlFor="ageRange" className="block text-sm font-semibold text-gray-600">
+							Edad
 						</label>
-						<select id="occasion" name="occasion" className="mt-1 w-full rounded-md border p-2 focus:outline-none">
-							<option value="1">Aniversario</option>
-							<option value="2">Cumpleaños</option>
-							<option value="3">Boda</option>
-							<option value="4">Graduación</option>
+						<select id="ageRange" name="ageRange" className="mt-1 w-full rounded-md border p-2 focus:outline-none">
+							{ages.map(({ label, value }) => (
+								<option key={value} value={value}>
+									{label}
+								</option>
+							))}
 						</select>
 					</div>
 					<div className="mb-4">
-						<label htmlFor="interest" className="block text-sm font-semibold text-gray-600">
-							Interés
+						<label htmlFor="events" className="block text-sm font-semibold text-gray-600">
+							Evento
 						</label>
-						<select id="interest" name="interest" className="mt-1 w-full rounded-md border p-2 focus:outline-none">
-							<option value="1">Tecnología</option>
-							<option value="2">Moda</option>
-							<option value="3">Deportes</option>
-							<option value="4">Hogar</option>
+						<select id="events" name="events" className="mt-1 w-full rounded-md border p-2 focus:outline-none">
+							{events.map(({ label, value }) => (
+								<option key={value} value={value}>
+									{label}
+								</option>
+							))}
 						</select>
 					</div>
 					<div className="mb-4">
-						<label htmlFor="more-about" className="block text-sm font-semibold text-gray-600">
+						<label htmlFor="hobbies" className="block text-sm font-semibold text-gray-600">
+							Hobbies
+						</label>
+						<select id="hobbies" name="hobbies" className="mt-1 w-full rounded-md border p-2 focus:outline-none">
+							<option value="musica">Música</option>
+							<option value="deportes">Deporte</option>
+							<option value="lectura">Lectura</option>
+							<option value="cocina">Cocina</option>
+							<option value="viajar">Viajar</option>
+							<option value="comer">Comer</option>
+						</select>
+					</div>
+					<div className="mb-4">
+						<label htmlFor="moreDetails" className="block text-sm font-semibold text-gray-600">
 							Más sobre la persona
 						</label>
 						<textarea
-							id="more-about"
-							name="more-about"
+							id="moreDetails"
+							name="moreDetails"
 							rows={3}
 							className="mt-1 h-32 max-h-96 min-h-32 w-full rounded-md border p-2 focus:outline-none"
 						/>
